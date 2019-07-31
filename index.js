0b2293ef7e5e255fc119574564ad558585f170d4
iampeterbanjo <peterbanjo@gmail.com>
2019-07-31 05:57:13 +0100

    rename exports. add example

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 const plugin = require('./plugin');
 
-module.exports = {
-	plugin,
-	options: {
-		accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-		captureUncaught: true,
-		captureUnhandledRejections: true,
-	},
-};
+module.exports = plugin;
